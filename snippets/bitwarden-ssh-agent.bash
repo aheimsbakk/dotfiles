7746e29b73d07caff42c0b9ec3aa4f6ca9387b7c
Arnulf Heimsbakk <8190865+aheimsbakk@users.noreply.github.com>
2026-03-22 02:13:45 +0100

    fix: bitwarden socket

--- snippets/bitwarden-ssh-agent.bash
+++ snippets/bitwarden-ssh-agent.bash
@@ -1,5 +1,15 @@
-if [ -S $HOME/.bitwarden-ssh-agent.sock ]; then
-  export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
-elif [ -S $HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock ]; then
-  export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+_set_bitwarden_ssh() {
+    if [ -S "$HOME/.bitwarden-ssh-agent.sock" ]; then
+        export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
+    elif [ -S "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock" ]; then
+        export SSH_AUTH_SOCK="$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
+    fi
+}
+
+if [ -z "$SSH_CONNECTION" ]; then
+    # Local session: Always try to use Bitwarden
+    _set_bitwarden_ssh
+elif [ -z "$SSH_AUTH_SOCK" ] || [ ! -S "$SSH_AUTH_SOCK" ]; then
+    # SSH session: Only use Bitwarden if no agent was forwarded
+    _set_bitwarden_ssh
 fi
