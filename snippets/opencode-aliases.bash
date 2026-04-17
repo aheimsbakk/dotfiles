c2e2130b3a765f835b5bcb457776fa5409ef5e3f
Arnulf Heimsbakk <8190865+aheimsbakk@users.noreply.github.com>
2026-04-17 19:52:56 +0200

    fedora compat

--- snippets/opencode-aliases.bash
+++ snippets/opencode-aliases.bash
@@ -4,6 +4,6 @@
 #
 
 if which podman > /dev/null; then
-    alias oc='podman run -e OPENCODE_CONFIG=/home/opencode/opencode.json --hostname vibe --name opencode --rm --userns=keep-id -ti -v opencode:/home/opencode -v "$PWD":/work -v "$HOME"/.gitconfig:/home/opencode/.gitconfig opencode:latest'
-    alias ocw='podman run -e OPENCODE_CONFIG=/home/opencode/opencode.json --hostname vibe --name opencode --rm --userns=keep-id -ti -p 4096:4096 -v opencode:/home/opencode -v "$PWD":/work -v "$HOME"/.gitconfig:/home/opencode/.gitconfig opencode:latest opencode-cli web --hostname 0.0.0.0'
+    alias oc='podman run -e OPENCODE_CONFIG=/home/opencode/opencode.json --hostname vibe --name opencode --rm --userns=keep-id -ti -v opencode:/home/opencode -v "$PWD":/work:Z -v "$HOME"/.gitconfig:/home/opencode/.gitconfig:Z opencode:latest'
+    alias ocw='podman run -e OPENCODE_CONFIG=/home/opencode/opencode.json --hostname vibe --name opencode --rm --userns=keep-id -ti -p 4096:4096 -v opencode:/home/opencode -v "$PWD":/work:Z -v "$HOME"/.gitconfig:/home/opencode/.gitconfig:Z opencode:latest opencode-cli web --hostname 0.0.0.0'
 fi
